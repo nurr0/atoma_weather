25c26b85c145167050d64d3739b55b57d3cfb95d
nurro <romannuriyev@gmail.com>
2023-03-31 19:13:41 +0600

    cors-allowed

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
 from settings import API_KEY
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    expose_headers=["*"],
+)
 
 cookies = {
     'is_gdpr': '0',
