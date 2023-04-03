fc2490ef2df6619a7cc8e418526c4e1404cd8382
nurro <romannuriyev@gmail.com>
2023-04-03 18:24:37 +0600

    fixed statuses

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import requests
 from bs4 import BeautifulSoup
 from settings import API_KEY
@@ -19,11 +19,13 @@ app.add_middleware(
 
 
 @app.get("/weather/now/")
-async def get_weather_by_coords(latitude, longitude, api_key):
+async def get_weather_by_coords(latitude, longitude, api_key, response: Response):
     if api_key == API_KEY:
         result = await get_weather(latitude, longitude)
+        response.status_code = 200
         return result
     else:
+        response.status_code = 403
         return {
             'status': '403',
             'descr': 'Доступ запрещен'
@@ -31,21 +33,24 @@ async def get_weather_by_coords(latitude, longitude, api_key):
 
 
 @app.get("/weather/city/")
-async def get_weather_by_city(city, api_key):
+async def get_weather_by_city(city, api_key, response: Response):
     if api_key == API_KEY:
         coords = await get_city_coord(city)
         if coords:
             latitude = coords[0]
             longitude = coords[1]
             result = await get_weather(latitude, longitude)
+            response.status_code = 200
             return result
 
         else:
+            response.status_code = 404
             return {
                 'status': '404',
                 'descr': 'Город не найден'
             }
     else:
+        response.status_code = 403
         return {
             'status': '403',
             'descr': 'Доступ запрещен'
